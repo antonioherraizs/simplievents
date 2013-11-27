1dc217d0fd3b8631c6af6f0b87677089013aac1d
Antonio Herraiz <herraiz@gmail.com>
2013-11-26 23:07:09 -0700

    add phonegap skeleton app

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/home/toni/stuff/projects/20131126-simplisafe_event_log_app/simplievents/platforms/android/gen/com/antonioherraizs/apps/R.java \
+ : /home/toni/stuff/projects/20131126-simplisafe_event_log_app/simplievents/platforms/android/res/xml/config.xml \
+/home/toni/stuff/projects/20131126-simplisafe_event_log_app/simplievents/platforms/android/res/drawable-ldpi/icon.png \
+/home/toni/stuff/projects/20131126-simplisafe_event_log_app/simplievents/platforms/android/res/values/strings.xml \
+/home/toni/stuff/projects/20131126-simplisafe_event_log_app/simplievents/platforms/android/res/drawable-hdpi/icon.png \
+/home/toni/stuff/projects/20131126-simplisafe_event_log_app/simplievents/platforms/android/res/drawable-xhdpi/icon.png \
+/home/toni/stuff/projects/20131126-simplisafe_event_log_app/simplievents/platforms/android/res/drawable/icon.png \
+/home/toni/stuff/projects/20131126-simplisafe_event_log_app/simplievents/platforms/android/res/drawable-mdpi/icon.png \
+/home/toni/stuff/projects/20131126-simplisafe_event_log_app/simplievents/platforms/android/bin/AndroidManifest.xml \
