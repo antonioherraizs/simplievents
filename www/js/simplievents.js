70554491b3f5c15aaf7bbaac29455e8a88b88481
Antonio Herraiz <herraiz@gmail.com>
2013-12-03 02:25:47 -0700

    Add login failed behavior

--- www/js/simplievents.js
+++ www/js/simplievents.js
@@ -15,6 +15,8 @@ $(function() {
 
       console.log('main(): loggedIn = ' + loggedIn);
       console.log('main(): cookies = ' + document.cookies);
+
+      // If value was stored, maybe cookies are still valid?
       if (loggedIn) {
         console.log('main(): already logged in, just refresh');
         getEvents();
@@ -24,7 +26,6 @@ $(function() {
       $('#button-submit').on('click', function(e) {
         
         loginAndGet();
-        location.href = "#page-events";
 
       });
 
@@ -38,6 +39,10 @@ $(function() {
 
       });
     });
+
+    function gotLoginPage( jquery_page ) {
+      return jquery_page.filter("title").text().substr(0,6) == "Log in";
+    }
 
     function loginAndGet() {
       $.ajax({
@@ -53,14 +58,22 @@ $(function() {
       })
         .done(function( data ) {
 
-          var milliseconds = (new Date).getTime();
-          var $r = $(data);
-          logURL = $r.filter("link[rel='canonical']").attr('href') + '/utility/tables?_=' + milliseconds + '&&table=event-log-table';
-          console.log("login(): loggedIn = true");
-          loggedIn = true;
+          var $response = $(data);
 
-          // call it here to prevent it from beng fired before we are logged in
-          getEvents();
+          if ( gotLoginPage($response) ) {
+            // user/pass wrong
+            console.log("loginAndGet(): user/pass wrong!");
+            loggedIn = false;
+          } else {
+            var milliseconds = (new Date).getTime();
+            logURL = $response.filter("link[rel='canonical']").attr('href')
+              + '/utility/tables?_=' + milliseconds + '&&table=event-log-table';
+            console.log("login(): loggedIn = true");
+            loggedIn = true;
+
+            // call it here to prevent it from beng fired before we are logged in
+            getEvents();
+          }
         })
         .fail(function( e ) {
           console.log( "Error " + e.responseStatus + ": " + e.responseText );
@@ -76,14 +89,27 @@ $(function() {
         .done(function( data ) {
 
           var $response = $(data);
-          var table = $response.filter('table').html();
-          $('#html5-block').html( "<table>" + table + "</table>" );
-          $('#html5-block a').remove();
-          $('#html5-block tr').removeClass();
 
-          // bootstrap3 table styling
-          $('#html5-block table').attr('class', 'table table-bordered table-condensed table-striped');
-          console.log("getEvents(): table reloaded");
+          if ( gotLoginPage($response) ) {
+            // send to login page, with a message
+            location.href = "#page-login";
+            console.log("getEvents(): not logged in! changed pass?");
+            loggedIn = false;
+          } else {
+            var table = $response.filter('table').html();
+            $('#html5-block').empty();
+            $('#html5-block').html( "<table>" + table + "</table>" );
+            $('#html5-block a').remove();
+            $('#html5-block tr').removeClass();
+
+            // bootstrap3 table styling
+            $('#html5-block table')
+              .attr('class', 'table table-bordered table-condensed table-striped');
+            console.log("getEvents(): table reloaded");
+
+            // and go to see the log, in case we're not there
+            location.href = "#page-events";
+          }
         })
         .fail(function( e ) {
           console.log( "Error " + e.responseStatus + ": " + e.responseText );
